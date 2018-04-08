0bf6801e3a27f77c1019e183e03a9f747f1c1483
scmilee <drudged@oregonstate.edu>
2018-04-08 15:23:38 -0700

    refactor park tests using let

--- app/models/National_park.rb
+++ app/models/National_park.rb
@@ -1,19 +1,19 @@
 class National_park
 
   attr_accessor :park_name
-  attr_accessor :trail_head_array
+  attr_accessor :trail_heads
 
   def initialize(park_name)
     @park_name = park_name
-    @trail_head_array = []
+    @trail_heads = []
   end
 
   def has_trailhead?
-    trail_head_array.length > 0
+    trail_heads.length > 0
   end
 
   def add_a_trailhead(trailObject)
-    trail_head_array << trailObject
+    trail_heads << trailObject
   end
 
 
--- spec/models/National_park_spec.rb
+++ spec/models/National_park_spec.rb
@@ -2,17 +2,18 @@ require "rails_helper"
 
 RSpec.describe National_park do
 
+  let (:park) {National_park.new('billsPark')}
+
   it "considers a National park to not have a trailhead" do
-    park = National_park.new('mikesPark')
     expect(park.has_trailhead?).to be_falsey
   end
 
   it "considers a national park to add a trailhead" do
-    park = National_park.new('billsPark')
-    trailhead_count = park.trail_head_array.length
+
+    trailhead_count = park.trail_heads.length
 
     park.add_a_trailhead("trailObject")
-    change_in_trailheads = park.trail_head_array.length - trailhead_count
+    change_in_trailheads = park.trail_heads.length - trailhead_count
 
 
     expect(park.has_trailhead?).to be_truthy
