9735df536d1ff5bbcc76006c6feca33ab620b93d
scmilee <drudged@oregonstate.edu>
2018-04-08 13:13:50 -0700

    add more coverage to add_a_trailhead method test

--- spec/models/National_park_spec.rb
+++ spec/models/National_park_spec.rb
@@ -9,8 +9,14 @@ RSpec.describe National_park do
 
   it "considers a national park to add a trailhead" do
     park = National_park.new('billsPark')
+    trailhead_count = park.trail_head_array.length
+
     park.add_a_trailhead("trailObject")
+    change_in_trailheads = park.trail_head_array.length - trailhead_count
+
+
     expect(park.has_trailhead?).to be_truthy
+    expect(change_in_trailheads).to be(1)
   end
-  
+
 end
