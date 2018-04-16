759c1d8836380551d07af819ae59b19f0d8a9249
drewjbartlett <drewb9@gmail.com>
2018-04-15 19:51:47 -0700

    Add failing test for add trail. Add errror message for trails controller

--- spec/system/add_trail_spec.rb
+++ spec/system/add_trail_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe "adding a trail", type: :system do
     expect(page).to have_content("Appalachian Trail") 
     expect(page).to have_content(8)
   end
+
+  it "allows a user to create a trail" do
+    visit new_trail_path
+
+    fill_in "Name", with: ""
+    
+    click_on("Create Trail")
+
+    expect(page).to have_content("Your trail must have a name") 
+    
+  end
 end
+
