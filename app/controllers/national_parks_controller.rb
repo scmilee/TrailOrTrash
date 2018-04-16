6a18cbff681d35c563e07450526f2e9274dc17a4
scmilee <drudged@oregonstate.edu>
2018-04-15 20:02:33 -0700

    add create method to national park controller

--- app/controllers/national_parks_controller.rb
+++ app/controllers/national_parks_controller.rb
@@ -2,4 +2,21 @@ class NationalParksController < ApplicationController
   def new
     @national_park = NationalPark.new
   end
+
+  def create
+
+    if (params[:trail][:name] != '')
+      @national_park = NationalPark.create(
+        name: params[:national_park][:name],
+        )
+      @national_park.save
+      redirect_to national_parks_path
+    else
+      flash[:error] = 'Your trail must have a name'
+    end
+  end
+
+  def index
+    @national_parks = National_parks.all
+  end
 end
