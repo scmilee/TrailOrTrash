9c4b712491d15853d9f9792342afb56753ce8473
drewjbartlett <drewb9@gmail.com>
2018-04-15 17:38:41 -0700

    Readd overwritten test for national parks

--- spec/models/national_park_spec.rb
+++ spec/models/national_park_spec.rb
@@ -1,5 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe NationalPark, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe NationalPark do
+
+  let (:park) {NationalPark.new('billsPark')}
+  let (:new_trail) {Trail.new('bills Trail', 345)}
+
+  it "considers a National park to not have a trail" do
+    expect(park.has_trail?).to be_falsey
+  end
+
+  it "should increment the trail count" do
+    expect{park.add_trail(new_trail)}.to change{park.number_of_trails}.by(1)
+  end
+
 end
