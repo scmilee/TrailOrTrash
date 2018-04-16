a9c01ba47b7ab4572b1e4c157475b3c1c55696c9
drewjbartlett <drewb9@gmail.com>
2018-04-15 19:34:12 -0700

    Add create new trail erb file. Add index for trails. Add create trail e2e test

---
+++ spec/system/add_trail_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "adding a trail", type: :system do
+  it "allows a user to create a trail" do
+    visit new_trail_path
+
+    fill_in "Name", with: "Appalachian Trail"
+    
+    click_on("Create Trail")
+    visit trails_path
+
+    expect(page).to have_content("Appalachian Trail") 
+    expect(page).to have_content(8)
+  end
+end
