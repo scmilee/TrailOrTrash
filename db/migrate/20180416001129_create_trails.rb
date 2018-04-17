839261fb68cb1998691114ae831b0bf2a9963b6c
scmilee <drudged@oregonstate.edu>
2018-04-16 22:46:15 -0700

    change db schema for trails, changed types and names

--- db/migrate/20180416001129_create_trails.rb
+++ db/migrate/20180416001129_create_trails.rb
@@ -2,9 +2,9 @@ class CreateTrails < ActiveRecord::Migration[5.1]
   def change
     create_table :trails do |t|
       t.string :name
-      t.integer :distance
-      t.string :latitude
-      t.string :longitude
+      t.integer :length
+      t.float :latitude
+      t.float :longitude
       t.integer :national_park_id
 
       t.timestamps
