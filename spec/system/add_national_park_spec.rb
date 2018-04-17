923d1c4baa978dc51f6d3ec0171a1be1033afdd5
scmilee <drudged@oregonstate.edu>
2018-04-16 19:59:01 -0700

    refactor national park spec Describe language

--- spec/system/add_national_park_spec.rb
+++ spec/system/add_national_park_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
-RSpec.describe "adding a national park", type: :system do
+RSpec.describe "Creating a national park,", type: :system do
   
-  it "allows a user to create a national park " do
+  it "allows a user to add a national park " do
     visit new_national_park_path
     fill_in "Name", with: "Bills Park"
     click_on ("Create National Park")
@@ -10,7 +10,7 @@ RSpec.describe "adding a national park", type: :system do
     expect(page).to have_content("Bills Park")
   end
   
-  it "does not allow a user to create a park without a name" do
+  it "does not allow a user to add a park without a name" do
     visit new_national_park_path
     fill_in "Name", with: ""
     click_on ("Create National Park")
