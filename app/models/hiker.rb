b1bfafb8c7bd68bcfc69172372ea893d4298bf2f
drewjbartlett <drewb9@gmail.com>
2018-04-25 15:51:07 -0700

    Add miles_hiked feature and test

--- app/models/hiker.rb
+++ app/models/hiker.rb
@@ -1,3 +1,8 @@
 class Hiker < ApplicationRecord
   has_and_belongs_to_many :trails, :join_table => :trails_hiked
+
+  def miles_hiked
+    trails.inject(0) {|sum, trail| sum + trail[:length]}
+  end
+
 end
--- spec/models/hiker_spec.rb
+++ spec/models/hiker_spec.rb
@@ -7,7 +7,24 @@ RSpec.describe Hiker, type: :model do
     )
   end
 
+  let (:trail) do
+    Trail.create(
+      name: 'Appalachian Trail',
+      length: 2189,
+      latitude: -23.232321323,
+      longitude: 14.232313234
+    )
+  end
+
   it 'can have a name' do
     expect(hiker.name).to eq 'Forrest Gump'
+  end
+
+  it 'can have a total miles hiked' do
+    expect(hiker.miles_hiked).to eq(0)
+
+    hiker.trails << trail
+
+    expect(hiker.miles_hiked).to eq(trail.length)
   end
 end
