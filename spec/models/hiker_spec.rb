4c1593080030e024da0a4dac6b5cc3d0b63c5837
scmilee <drudged@oregonstate.edu>
2018-04-29 13:03:19 -0700

    refactor naming of tests from can have to "has" in  hiker spec

--- spec/models/hiker_spec.rb
+++ spec/models/hiker_spec.rb
@@ -6,14 +6,6 @@ RSpec.describe Hiker do
       name: 'Forrest Gump'
       )
   end
-  let (:trail) do
-    Trail.create(
-      name: 'Appalachian Trail',
-      length: 2189,
-      latitude: -23.232321323,
-      longitude: 14.232313234
-      )
-  end
 
   let (:trail) do
     Trail.create(
@@ -24,7 +16,7 @@ RSpec.describe Hiker do
       )
   end
 
-  it 'can have a name' do
+  it 'has a name' do
     expect(hiker.name).to eq 'Forrest Gump'
   end
 
@@ -33,23 +25,24 @@ RSpec.describe Hiker do
 
     hiker.trails << trail
     expect(hiker.number_of_trails_hiked).to eq(1)
-
-    it 'can have a total miles hiked' do
-      expect(hiker.miles_hiked).to eq(0)
-
-      hiker.trails << trail
-
-      expect(hiker.miles_hiked).to eq(trail.length)
-    end
-
-    it 'has a Baby rank if no trails have been hiked' do
-      expect(hiker).to be_of_rank('Baby')
-    end
-
-    it 'has a Baby rank if no trails have been hiked' do
-      hiker.trails << trail
-
-      expect(hiker).to be_of_rank('Nuts')
-
-    end
   end
+
+  it 'has a total miles hiked' do
+    expect(hiker.miles_hiked).to eq(0)
+
+    hiker.trails << trail
+
+    expect(hiker.miles_hiked).to eq(trail.length)
+  end
+
+  it 'has a Baby rank if no trails have been hiked' do
+    expect(hiker).to be_of_rank('Baby')
+  end
+
+  it 'has a Baby rank if no trails have been hiked' do
+    hiker.trails << trail
+
+    expect(hiker).to be_of_rank('Nuts')
+
+  end
+end
