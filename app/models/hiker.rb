b31f719ac6aea7c8362373f512383967be82bb08
scmilee <drudged@oregonstate.edu>
2018-04-29 13:02:46 -0700

    fix missing end in hiker.rb

--- app/models/hiker.rb
+++ app/models/hiker.rb
@@ -4,34 +4,36 @@ class Hiker < ApplicationRecord
 
   def number_of_trails_hiked
     return trails.length
+  end
 
-    def miles_hiked
-      trails.inject(0) {|sum, trail| sum + trail[:length]}
-    end
+  def miles_hiked
+    trails.inject(0) {|sum, trail| sum + trail[:length]}
+  end
 
-    def rank 
-      ranks = [
-        { :name => "Baby", :threshold => 100 },
-        { :name => "Chump", :threshold => 500 },
-        { :name => "Decent", :threshold => 1000 },
-        { :name => "Baller", :threshold => 2000 },
-        { :name => "Nuts", :threshold => 5000 },
-      ]
+  def rank 
+    ranks = [
+      { :name => "Baby", :threshold => 100 },
+      { :name => "Chump", :threshold => 500 },
+      { :name => "Decent", :threshold => 1000 },
+      { :name => "Baller", :threshold => 2000 },
+      { :name => "Nuts", :threshold => 5000 },
+    ]
 
-      miles_hiked = self.miles_hiked
-      rank_title = ranks[0][:name]
+    miles_hiked = self.miles_hiked
+    rank_title = ranks[0][:name]
 
-      ranks.each do |rank| 
-        if (rank[:threshold] == ranks[ranks.length - 1][:threshold] && miles_hiked >= rank[:threshold])
-          rank_title = rank[:name]
-          break
-        elsif (miles_hiked <= rank[:threshold])
-          rank_title = rank[:name]
-          break
-        end
+    ranks.each do |rank| 
+      if (rank[:threshold] == ranks[ranks.length - 1][:threshold] && miles_hiked >= rank[:threshold])
+        rank_title = rank[:name]
+        break
+      elsif (miles_hiked <= rank[:threshold])
+        rank_title = rank[:name]
+        break
       end
-
-      rank_title.to_s
-      
-
     end
+
+    rank_title.to_s
+
+
+  end
+end
