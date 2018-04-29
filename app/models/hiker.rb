a3232ee914c9490dbe00495e94e23c9372add6d8
scmilee <drudged@oregonstate.edu>
2018-04-29 10:07:53 -0700

    add hiker test to test if you can add trails to a hiker object

--- app/models/hiker.rb
+++ app/models/hiker.rb
@@ -1,3 +1,8 @@
 class Hiker < ApplicationRecord
   has_and_belongs_to_many :trails, :join_table => :trails_hiked
+
+  def number_of_trails_hiked
+    return trails.length
+  end
+
 end
--- spec/models/hiker_spec.rb
+++ spec/models/hiker_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Hiker, type: :model do
+RSpec.describe Hiker do
   let (:hiker) do
     Hiker.create(
       name: 'Forrest Gump'
-    )
+      )
+  end
+  let (:trail) do
+    Trail.create(
+      name: 'Appalachian Trail',
+      length: 2189,
+      latitude: -23.232321323,
+      longitude: 14.232313234
+      )
   end
 
   it 'can have a name' do
     expect(hiker.name).to eq 'Forrest Gump'
+  end
+
+  it 'can add a trail to its hiked trails' do 
+
+    hiker.trails << trail
+    expect(hiker.number_of_trails_hiked).to eq(1)
+
   end
 end
