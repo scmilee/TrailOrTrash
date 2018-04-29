c022451b8e79a2c551ff2cf2bea9a2a3f3aa9e2f
scmilee <drudged@oregonstate.edu>
2018-04-29 13:05:31 -0700

    add TDD code for trails, that allows the tests to pass

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -2,4 +2,29 @@
 class Trail < ApplicationRecord
   belongs_to :national_park, optional: true
   has_and_belongs_to_many :hikers, :join_table => :trails_hiked
+
+  def difficulty
+    self.compute_difficulty
+  end
+
+  def compute_difficulty
+
+    if self.length < 100
+      'Easy'
+    elsif self.length > 100 && self.length < 500
+      'Medium'
+    else
+      'Hard'
+    end
+
+  end
+
+  def add_a_hiker(hiker)
+    self.hikers << hiker
+  end
+
+  def hiker_log
+    self.hikers
+  end
+
 end
